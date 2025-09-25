d350648a357ef4f9f63853839510a3d3316ac4eb
Nathaneil2025 <seanbad85@gmail.com>
2025-09-25 23:49:32 +0300

    changing100

--- infra/apim.tf
+++ infra/apim.tf
@@ -1,14 +1,10 @@
-# -------------------
-# API Management
-# -------------------
 resource "azurerm_api_management" "chatbot_apim" {
   name                = "chatbot-apim-2025"
   location            = var.location
   resource_group_name = var.resource_group_name
   publisher_name      = "Chatbot Team"
   publisher_email     = "admin@example.com"
-
-  sku_name = "Developer_1" # For dev/test only
+  sku_name            = "Developer_1"
 
   virtual_network_type = "External"
   virtual_network_configuration {
@@ -25,13 +21,10 @@ resource "azurerm_api_management" "chatbot_apim" {
 
   depends_on = [
     azurerm_subnet_network_security_group_association.apim_assoc,
-    azurerm_subnet.apim_subnet
+    azurerm_subnet_route_table_association.apim_subnet_rt
   ]
 }
 
-# -------------------
-# API Definition
-# -------------------
 resource "azurerm_api_management_api" "chatbot_api" {
   name                = "chatbot-api"
   resource_group_name = var.resource_group_name
@@ -43,14 +36,10 @@ resource "azurerm_api_management_api" "chatbot_api" {
 
   import {
     content_format = "swagger-link-json"
-    # Dummy placeholder for now; CI/CD replaces this later
     content_value  = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/examples/v3.0/petstore.json"
   }
 }
 
-# -------------------
-# API Policy
-# -------------------
 resource "azurerm_api_management_api_policy" "rate_limit_policy" {
   api_name            = azurerm_api_management_api.chatbot_api.name
   api_management_name = azurerm_api_management.chatbot_apim.name
@@ -76,127 +65,3 @@ XML
     azurerm_api_management_api.chatbot_api
   ]
 }
-
-# -------------------
-# APIM NSG
-# -------------------
-resource "azurerm_network_security_group" "apim_nsg" {
-  name                = "apim-nsg"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  # Inbound rules
-  security_rule {
-    name                       = "AllowClientInbound"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_address_prefix      = "Internet"
-    source_port_range          = "*"
-    destination_address_prefix = "VirtualNetwork"
-    destination_port_ranges    = ["80", "443"]
-  }
-
-  security_rule {
-    name                       = "AllowMgmtInbound"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_address_prefix      = "ApiManagement"
-    source_port_range          = "*"
-    destination_address_prefix = "VirtualNetwork"
-    destination_port_range     = "3443"
-  }
-
-  security_rule {
-    name                       = "AllowAzureLBInbound"
-    priority                   = 120
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_address_prefix      = "AzureLoadBalancer"
-    source_port_range          = "*"
-    destination_address_prefix = "VirtualNetwork"
-    destination_port_range     = "6390"
-  }
-
-  security_rule {
-    name                       = "AllowTrafficManagerInbound"
-    priority                   = 130
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_address_prefix      = "AzureTrafficManager"
-    source_port_range          = "*"
-    destination_address_prefix = "VirtualNetwork"
-    destination_port_range     = "443"
-  }
-
-  # Outbound rules
-  security_rule {
-    name                       = "AllowOutboundInternet"
-    priority                   = 200
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_address_prefix      = "VirtualNetwork"
-    source_port_range          = "*"
-    destination_address_prefix = "Internet"
-    destination_port_range     = "80"
-  }
-
-  security_rule {
-    name                       = "AllowOutboundStorage"
-    priority                   = 210
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_address_prefix      = "VirtualNetwork"
-    source_port_range          = "*"
-    destination_address_prefix = "Storage"
-    destination_port_range     = "443"
-  }
-
-  security_rule {
-    name                       = "AllowOutboundSQL"
-    priority                   = 220
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_address_prefix      = "VirtualNetwork"
-    source_port_range          = "*"
-    destination_address_prefix = "SQL"
-    destination_port_range     = "1433"
-  }
-
-  security_rule {
-    name                       = "AllowOutboundKeyVault"
-    priority                   = 230
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_address_prefix      = "VirtualNetwork"
-    source_port_range          = "*"
-    destination_address_prefix = "AzureKeyVault"
-    destination_port_range     = "443"
-  }
-
-  security_rule {
-    name                       = "AllowOutboundAzureMonitor"
-    priority                   = 240
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_address_prefix      = "VirtualNetwork"
-    source_port_range          = "*"
-    destination_address_prefix = "AzureMonitor"
-    destination_port_ranges    = ["1886", "443"]
-  }
-
-  tags = {
-    environment = "chatbot"
-  }
-}
-
--- infra/networking.tf
+++ infra/networking.tf
@@ -16,15 +16,13 @@ resource "azurerm_subnet" "aks_subnet" {
   address_prefixes     = ["192.168.0.0/24"]
 }
 
-# -------------------
-# Subnet Delegation (critical for APIM)
-# -------------------
 resource "azurerm_subnet" "apim_subnet" {
   name                 = "apim-subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.main_vnet.name
   address_prefixes     = ["192.168.1.0/24"]
 
+  # APIM requires delegation
   delegation {
     name = "apim_delegation"
     service_delegation {
@@ -34,7 +32,6 @@ resource "azurerm_subnet" "apim_subnet" {
   }
 }
 
-
 resource "azurerm_subnet" "data_subnet" {
   name                 = "data-subnet"
   resource_group_name  = var.resource_group_name
@@ -42,6 +39,105 @@ resource "azurerm_subnet" "data_subnet" {
   address_prefixes     = ["192.168.2.0/24"]
 }
 
+# NSG for APIM
+resource "azurerm_network_security_group" "apim_nsg" {
+  name                = "apim-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  # client traffic
+  security_rule {
+    name                       = "AllowClientInbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "VirtualNetwork"
+    destination_port_ranges    = ["80", "443"]
+    source_port_range          = "*"
+  }
+
+  # control plane
+  security_rule {
+    name                       = "AllowMgmtInbound"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_address_prefix      = "ApiManagement"
+    destination_address_prefix = "VirtualNetwork"
+    destination_port_ranges    = ["3443"]
+    source_port_range          = "*"
+  }
+
+  # required Azure service dependencies
+  security_rule {
+    name                       = "AllowOutboundInternet"
+    priority                   = 200
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "Internet"
+    destination_port_range     = "80"
+    source_port_range          = "*"
+  }
+
+  security_rule {
+    name                       = "AllowOutboundStorage"
+    priority                   = 210
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "Storage"
+    destination_port_range     = "443"
+    source_port_range          = "*"
+  }
+
+  security_rule {
+    name                       = "AllowOutboundSQL"
+    priority                   = 220
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "SQL"
+    destination_port_range     = "1433"
+    source_port_range          = "*"
+  }
+
+  security_rule {
+    name                       = "AllowOutboundKeyVault"
+    priority                   = 230
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "AzureKeyVault"
+    destination_port_range     = "443"
+    source_port_range          = "*"
+  }
+
+  security_rule {
+    name                       = "AllowOutboundAzureMonitor"
+    priority                   = 240
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "AzureMonitor"
+    destination_port_ranges    = ["443", "1886"]
+    source_port_range          = "*"
+  }
+
+  tags = {
+    environment = "chatbot"
+  }
+}
+
+# Associate NSG + Route Table with APIM subnet
 resource "azurerm_subnet_network_security_group_association" "apim_assoc" {
   subnet_id                 = azurerm_subnet.apim_subnet.id
   network_security_group_id = azurerm_network_security_group.apim_nsg.id
