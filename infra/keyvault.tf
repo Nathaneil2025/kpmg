52231ad0032c5a178865a1a6db87f390fcf7ddb5
Nathaneil2025 <seanbad85@gmail.com>
2025-09-25 18:55:28 +0300

    more resources1

--- infra/keyvault.tf
+++ infra/keyvault.tf
@@ -7,8 +7,8 @@ resource "azurerm_key_vault" "chatbot_kv" {
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
 
-  purge_protection_enabled       = true
-  public_network_access_enabled  = true
+  purge_protection_enabled      = true
+  public_network_access_enabled = true
 
   tags = {
     environment = "chatbot"
@@ -20,6 +20,8 @@ resource "azurerm_role_assignment" "workload_kv_secrets" {
   principal_id         = azurerm_user_assigned_identity.workload_identity.principal_id
   role_definition_name = "Key Vault Secrets User"
   scope                = azurerm_key_vault.chatbot_kv.id
+
+  depends_on = [azurerm_key_vault.chatbot_kv]
 }
 
 # Role Assignment: allow GitHub service principal full Key Vault admin rights
@@ -27,6 +29,8 @@ resource "azurerm_role_assignment" "github_kv_admin" {
   principal_id         = "4e4f585b-62da-4b84-88cd-8e247f841622" # GitHub Actions SP objectId
   role_definition_name = "Key Vault Administrator"
   scope                = azurerm_key_vault.chatbot_kv.id
+
+  depends_on = [azurerm_key_vault.chatbot_kv]
 }
 
 # Role Assignment: allow GitHub service principal to manage certs
@@ -34,6 +38,8 @@ resource "azurerm_role_assignment" "github_kv_certificates" {
   principal_id         = "4e4f585b-62da-4b84-88cd-8e247f841622" # GitHub Actions SP objectId
   role_definition_name = "Key Vault Certificates Officer"
   scope                = azurerm_key_vault.chatbot_kv.id
+
+  depends_on = [azurerm_key_vault.chatbot_kv]
 }
 
 # Role Assignment: allow GitHub service principal to read secrets
@@ -41,4 +47,6 @@ resource "azurerm_role_assignment" "github_kv_secrets" {
   principal_id         = "4e4f585b-62da-4b84-88cd-8e247f841622" # GitHub Actions SP objectId
   role_definition_name = "Key Vault Secrets User"
   scope                = azurerm_key_vault.chatbot_kv.id
+
+  depends_on = [azurerm_key_vault.chatbot_kv]
 }
