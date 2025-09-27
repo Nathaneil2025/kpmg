dc5093a80bade27888891b32810db7bb74e78ec4
Nathaneil2025 <seanbad85@gmail.com>
2025-09-27 19:45:53 +0300

    chdas

--- infra/staticweb.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_static_site" "chatbot_frontend" {
-  name                = "chatbot-frontend-2025"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  sku_tier            = "Free"
-}
