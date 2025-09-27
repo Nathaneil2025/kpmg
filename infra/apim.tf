c2d704ef394e7189958c26c2608b983998356b68
Nathaneil2025 <seanbad85@gmail.com>
2025-09-27 23:29:38 +0300

    swf3

--- infra/apim.tf
+++ infra/apim.tf
@@ -1,4 +1,4 @@
-# API Management Service - Remove VNet Integration
+# API Management Service - No VNet Integration
 resource "azurerm_api_management" "chatbot_apim" {
   name                = "chatbot-apim-2025"
   location            = var.location
@@ -7,12 +7,6 @@ resource "azurerm_api_management" "chatbot_apim" {
   publisher_email     = "admin@example.com"
   sku_name            = "Developer_1"
 
-  # Remove these lines completely:
-  # virtual_network_type = "External"
-  # virtual_network_configuration {
-  #   subnet_id = azurerm_subnet.apim_subnet.id
-  # }
-
   identity {
     type = "SystemAssigned"
   }
@@ -20,10 +14,107 @@ resource "azurerm_api_management" "chatbot_apim" {
   tags = {
     environment = "chatbot"
   }
+}
 
-  # Remove VNet dependencies:
-  # depends_on = [
-  #   azurerm_subnet_network_security_group_association.apim_assoc,
-  #   azurerm_subnet_route_table_association.apim_subnet_rt
-  # ]
+# Backend Service pointing to Application Gateway PUBLIC IP
+resource "azurerm_api_management_backend" "chatbot_appgw_backend" {
+  name                = "chatbot-appgw-backend"
+  api_management_name = azurerm_api_management.chatbot_apim.name
+  resource_group_name = var.resource_group_name
+
+  protocol = "http"
+  url      = "http://${azurerm_public_ip.appgw_public_ip.ip_address}"
+
+  depends_on = [
+    azurerm_api_management.chatbot_apim,
+    azurerm_application_gateway.chatbot_appgw
+  ]
+}
+
+# API Definition
+resource "azurerm_api_management_api" "chatbot_api" {
+  name                = "chatbot-api"
+  resource_group_name = var.resource_group_name
+  api_management_name = azurerm_api_management.chatbot_apim.name
+  revision            = "1"
+  display_name        = "Chatbot API"
+  path                = "chat"
+  protocols           = ["https"]
+
+  depends_on = [azurerm_api_management.chatbot_apim]
+}
+
+# Chat Operation - POST /chat
+resource "azurerm_api_management_api_operation" "chat_post" {
+  operation_id        = "chat-post"
+  api_name           = azurerm_api_management_api.chatbot_api.name
+  api_management_name = azurerm_api_management.chatbot_apim.name
+  resource_group_name = var.resource_group_name
+  display_name       = "Chat with Bot"
+  method             = "POST"
+  url_template       = "/chat"
+  description        = "Send message to chatbot"
+
+  request {
+    description = "Chat request"
+
+    representation {
+      content_type = "application/json"
+      example {
+        name  = "example"
+        value = jsonencode({
+          session_id = "testing"
+          message    = "What is the capital of Israel?"
+        })
+      }
+    }
+  }
+
+  response {
+    status_code  = 200
+    description  = "Successful response"
+
+    representation {
+      content_type = "application/json"
+    }
+  }
+
+  depends_on = [azurerm_api_management_api.chatbot_api]
+}
+
+# Policy - Routes to Application Gateway Backend
+resource "azurerm_api_management_api_policy" "chatbot_routing_policy" {
+  api_name            = azurerm_api_management_api.chatbot_api.name
+  api_management_name = azurerm_api_management.chatbot_apim.name
+  resource_group_name = var.resource_group_name
+
+  xml_content = <<XML
+<policies>
+  <inbound>
+    <rate-limit calls="10" renewal-period="60" />
+    <set-backend-service backend-id="chatbot-appgw-backend" />
+    <rewrite-uri template="/" copy-unmatched-params="true" />
+    <set-header name="Host" exists-action="override">
+      <value>chatbot.kpmg.local</value>
+    </set-header>
+    <base />
+  </inbound>
+  <backend>
+    <base />
+  </backend>
+  <outbound>
+    <base />
+  </outbound>
+  <on-error>
+    <base />
+  </on-error>
+</policies>
+XML
+
+  depends_on = [
+    azurerm_api_management.chatbot_apim,
+    azurerm_api_management_api.chatbot_api,
+    azurerm_api_management_backend.chatbot_appgw_backend,
+    azurerm_api_management_api_operation.chat_post
+  ]
 }
